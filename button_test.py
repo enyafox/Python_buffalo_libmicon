fc09f256312c4fb3b4371327fe178fc1e4e963dc
enyafox <67861653+enyafox@users.noreply.github.com>
2021-05-20 23:42:20 +0100

    Update button_test.py

--- button_test.py
+++ button_test.py
@@ -1,26 +1,50 @@
 #!/usr/bin/python3
 
-import libmicon
-import time
+##simple example of polling one button and taking an action if it is held down.
+import libmicon, time
 
-print("")
-print("Press Buttons on the Device and watch the ouput")
-print("Enter CTRL-C to quit")
-print("")
-
-oldstate=0
 while True:
 	test = libmicon.micon_api("/dev/ttyS1")
-	state=test.send_read_cmd(0x36)
-	if oldstate != state:
-		print("Button(s): ", end='')
-		for button_num in range(8):
-			if state[0] & (2**button_num) == 0:
-				print(2**button_num," ", end='')
-		print("")
-
-	oldstate=state
+	state=int.from_bytes(test.send_read_cmd(0x36),byteorder="big")
+	##print button number
+	print(state)
+	test.port.close()
 	time.sleep(0.5)
-
-test.port.close()
 quit()
+
+
+"""What you could do is use this to read a simple number, for example, the TS-XL models, unpressed is 31, the power button is 30, 
+the function button is 29, display is 17 and the hidden reset button is 23.
+You can use this to build a script using that code above,
+
+import libmicon, time, os
+
+press_time=2
+poll_speed=0.5 ##currently this is fast as we can reliably poll
+tickcnt=0
+
+while True:
+	test = libmicon.micon_api("/dev/ttyS1")
+	state=int.from_bytes(test.send_read_cmd(0x36),byteorder="big")
+	##if button held down start counting
+	if state == 30:
+		tickcnt=tickcnt +1
+		if tickcnt > (press_time/poll_speed):
+			test.set_lcd_buffer(0x90,"Shutting Down!!"," ")
+			test.cmd_force_lcd_disp(libmicon.lcd_disp_buffer0)
+			test.send_write_cmd(1,libmicon.lcd_set_dispitem,0x20)
+			test.set_lcd_color(libmicon.LCD_COLOR_RED)
+			test.set_lcd_brightness(libmicon.LCD_BRIGHT_FULL)
+			test.cmd_sound(libmicon.BZ_MUSIC1)
+			test.port.close() ##if we do something other than shutdown we'd probably skip this
+			os.system('shutdown -h 0')
+			tickcnt=0
+
+	##reset counter if released
+	else:
+		tickcnt=0
+
+	test.port.close()
+	##currently this is fast as we can reliably poll
+	time.sleep(poll_speed)
+quit()"""
